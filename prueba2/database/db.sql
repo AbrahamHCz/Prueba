6482605f8474d234b3f29f8b2c6138ddf4f6b4fa
AbrahamZ <miguelh.goa@gmail.com>
2022-04-28 11:11:33 -0600

    db script fix

--- prueba2/database/db.sql
+++ prueba2/database/db.sql
@@ -3,7 +3,7 @@ create database prueba;
 use prueba;
 
 create table test (
-coded INT primary key auto_increment not null,
+code INT(12) primary key auto_increment not null,
 a int(12) not null,
 b varchar(12) not null
 );
